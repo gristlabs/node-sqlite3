852e9b5caa6a5ca320b045753095083a238a59d7
Janet Vorobyeva <janet.vorobyeva@gmail.com>
2018-06-12 12:45:36 -0400

    Fixed testing on linux, readme, electron test
    marshal.node failed to link on linux for marshal-test.js
    Electron test only worked if repo was checked out as exactly node-sqlite3

--- test/cpp/binding.gyp
+++ test/cpp/binding.gyp
@@ -3,6 +3,7 @@
     {
         "cflags" : ["-Wall", "-Wextra", "-Wno-unused-parameter"],
         "defines": [ "V8_DEPRECATION_WARNINGS=1" ],
+        "libraries": [ "../../../build/Release/node_sqlite3.node" ],
         "include_dirs": ["<!(node -e \"require('../..')\")", "<!(node -e \"require('nan')\")"],
     },
   "targets": [
--- test/electron.test.js
+++ test/electron.test.js
@@ -4,6 +4,6 @@ describe('electron', function() {
   it('respects ELECTRON_VERSION', function() {
     process.env.ELECTRON_VERSION = '1.2.3';
     assert.throws(function() { require('..'); },
-                  (/Cannot find module .*\/node-sqlite3\/lib\/binding\/electron-v1.2-[^-]+-x64\/node_sqlite3.node/));
+                  (/Cannot find module .*\/lib\/binding\/electron-v1.2-[^-]+-x64\/node_sqlite3.node/));
   });
 });
