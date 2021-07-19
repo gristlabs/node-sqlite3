1d4c2f5931384ba20b647da6312095b8d8ac46a3
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2021-07-19 11:24:51 -0400

    turn off support for ATTACH
    This sets the maximum number of ATTACHed databases to 0, as suggested
    by https://www.sqlite.org/security.html

---
+++ test/attach.test.js
@@ -0,0 +1,24 @@
+var sqlite3 = require('..');
+var helper = require('./support/helper');
+
+describe('attach', function() {
+  // Check that ATTACH is not supported, as part of defense in depth measures.
+  it ('does not permit attaching another db', function(done) {
+    helper.deleteFile('test/tmp/test1.db');
+    helper.deleteFile('test/tmp/test2.db');
+    var db = new sqlite3.Database('test/tmp/test1.db', function(err) {
+      if (err) throw err;
+      db.exec("ATTACH 'test/support/prepare.db' AS zing", function (err) {
+        if (!err) {
+          throw new Error('ATTACH should not succeed');
+        }
+        if (err.errno === sqlite3.ERROR &&
+            err.message === 'SQLITE_ERROR: too many attached databases - max 0') {
+          db.close(done);
+        } else {
+          throw err;
+        }
+      });
+    });
+  });
+});
