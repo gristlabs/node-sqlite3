1c5e22de7859058eb23cb9d6c5377f0b07d12ccc
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2021-07-19 11:33:01 -0400

    remove unneeded file delete; bump version

--- test/attach.test.js
+++ test/attach.test.js
@@ -4,9 +4,8 @@ var helper = require('./support/helper');
 describe('attach', function() {
   // Check that ATTACH is not supported, as part of defense in depth measures.
   it ('does not permit attaching another db', function(done) {
-    helper.deleteFile('test/tmp/test1.db');
-    helper.deleteFile('test/tmp/test2.db');
-    var db = new sqlite3.Database('test/tmp/test1.db', function(err) {
+    helper.deleteFile('test/tmp/test_attach.db');
+    var db = new sqlite3.Database('test/tmp/test_attach.db', function(err) {
       if (err) throw err;
       db.exec("ATTACH 'test/support/prepare.db' AS zing", function (err) {
         if (!err) {
